06eb1dc1cf125b0ab34fd858e985311e7d51aa5c
Oliver Steele <steele@osteele.com>
2016-02-22 21:54:29 -0500

    teach diff_answers about cells that don't have outputs

--- tools/diff_answers.py
+++ tools/diff_answers.py
@@ -8,23 +8,21 @@ import json
 import os
 import sys
 from multiprocessing import Pool
-import urllib
 from extract_answers_template import get_user_repo_urls, get_user_notebook_urls, read_json_from_url
 
-def get_output_string(output):
-    lines = None
-    try:
-        lines = output['text']
-    except KeyError:
-        pass
-    try:
-        lines = output['data']['text/plain']
-    except KeyError:
-        pass
-    return ''.join(lines) if lines else None
 
-def make_cell_index(cell):
-    return (''.join(cell['source']), ''.join(get_output_string(output) for output in cell.get('outputs', [])))
+def get_output_string(output):
+    """Return a Jupyter cell's output as a single string; or None."""
+    lines = []
+    lines.extend(output.get('text', []))
+    lines.extend(output.get('data', {}).get('text/plain', []))
+    return ''.join(lines)
+
+
+def get_cell_eq_key(cell):
+    """Return an non-mutable object that can be used to distinguish two cells based on their inputs and outputs."""
+    return ''.join(cell.get('source', [])), ''.join(map(get_output_string, cell.get('outputs', [])))
+
 
 def cell_is_keeper(cell):
     return bool(cell['metadata'].get('is_question', None))
@@ -40,7 +38,7 @@ if __name__ == '__main__':
     with open(template_nb_path) as f:
         template = json.load(f)
 
-    template_cell_indices = set(make_cell_index(cell)
+    template_cell_indices = set(get_cell_eq_key(cell)
                                 for cell in template['cells']
                                 if not cell_is_keeper(cell))
 
@@ -48,8 +46,8 @@ if __name__ == '__main__':
     for notebook_url, nb in zip(notebook_urls, p.map(read_json_from_url, notebook_urls)):
         if not nb:
             continue
-        github_username = notebook_url.split('/')[3] # FIXME
-        nb['cells'] = [cell for cell in nb['cells'] if make_cell_index(cell) not in template_cell_indices]
+        github_username = notebook_url.split('/')[3]  # TODO preserve the name from the list of users
+        nb['cells'] = [cell for cell in nb['cells'] if get_cell_eq_key(cell) not in template_cell_indices]
         root, ext = os.path.splitext(os.path.basename(template_nb_path))
         output_path = 'processed_notebooks/{}_{}{}'.format(root, github_username, ext)
         print 'writing', output_path
